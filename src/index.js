7a2ae28271d41831f52ea9675a7d3eb1a7c07928
Pan Artur <artur.pankov.16@list.ru>
2020-02-24 20:25:16 +0300

    feat: add final resolution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let res  = '';
+    let nToStr = n.toString();
+
+    for (let i = nToStr.length - 1; i >= 0; i -= 1) {
+        if (nToStr[i] !== '-') {
+            res += nToStr[i];
+        }
+    }
+
+    return +res;
 }
